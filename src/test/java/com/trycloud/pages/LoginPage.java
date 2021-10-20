ed733e90c0984e65f946446415f3559435b77118
AbbosB12 <abbosboev@gmail.com>
2021-10-20 18:42:54 -0400

    Created TryCloud project.

---
+++ src/test/java/com/trycloud/pages/LoginPage.java
@@ -0,0 +1,44 @@
+package com.trycloud.pages;
+
+import com.trycloud.utilities.ConfigReader;
+import com.trycloud.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
+public class LoginPage {
+
+    @FindBy(id = "user")
+   public WebElement username;
+
+    @FindBy(id = "password")
+    public WebElement password;
+
+    @FindBy(id = "submit-form")
+    public WebElement login;
+
+
+    @FindBy(xpath = "//p[normalize-space(.)='Wrong username or password.']")
+    public WebElement errorMsg;
+
+    @FindBy()
+    public List<WebElement> modules;
+
+    public LoginPage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    public void goTo(){
+Driver.getDriver().get(ConfigReader.read("trycloud_url"));
+    }
+
+    public void login(String username,String password){
+        this.username.sendKeys(username);
+        this.password.sendKeys(password);
+        this.login.click();
+    }
+
+
+}
---
+++ src/test/resources/features/fileUpload.feature
@@ -0,0 +1,16 @@
+#5. Story: As a user, I should be able to able to access to Files module.
+
+#AC #1 - verify users can remove a file from favorites.
+#1. Login as a user
+#2. Click action-icon from any file on the page
+#3. Click “Remove from Favorites” option
+#4. Verify that the file is removed from Favorites sub-module’s table.
+#(Pre-condition: there should be at least 1 file is added to favorites table)
+
+#AC #2 - verify users can upload a file directly to the homepage (File-Uploading)
+#1. Login as a user
+#2. Click the “+” icon on top
+#3. Click “upload file”
+#4. Upload a file
+#5. Verify the file is displayed on the page
+  Feature: 
